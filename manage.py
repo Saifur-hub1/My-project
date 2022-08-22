ceb3847f21a261290605c107bc9c82c650f30b5c
root <root@localhost>
2022-08-22 13:17:44 +0600

    auto publish 1661152664015

---
+++ manage.py
@@ -0,0 +1,48 @@
+
+# AREA OF SHAPE code block
+
+base = float(input("Enter base ="))
+height = float(input("Enter Height ="))
+area = 0.5*base*height
+print("Your Triangle's area =",area)
+
+
+#MATH RELATED LIBRARY FUNCTION code block
+#------------------------------------
+
+print(max(20,10))
+print(min(20,10))
+print(pow(2,3))
+print(abs(-4))
+
+print(round(3.5))
+"""এখানে print (floor) ও print (ceil)
+ও print (sqrt) আসছে না।"""
+
+#Formatted string code block
+
+num = 20.5
+print(type(num))
+num = 20
+print(type(num))
+print("Saifur Rahman Mobile number : ",end =" ")
+print("01869560704")
+num1 = input("1st value =")
+num2 = input("2nd value =")
+num = int(num1)+int(num2)
+print("Sum is =",num)
+
+
+"""
+While loop uses
+"""
+
+Digit = 1
+while Digit < 100:
+  print("Saifur Rahman")
+  Digit = Digit +1
+
+
+
+
+
